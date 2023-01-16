b582448efb5c4f490a8e12cefe7ceddb750bc6cf
eobcre <eobcre@gmail.com>
2023-01-16 05:13:40 -0500

    react router settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
 
-export const App = () => {
-  return <div className='App'></div>;
+const App = () => {
+  // JSX
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 };
+
+export default App;
