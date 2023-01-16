912c2693e5ac66cb7e092e5cf3e35a55b1d9b3d5
eobcre <eobcre@gmail.com>
2023-01-16 06:00:14 -0500

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
 import { Home } from './components/Home';
+import { Salon } from './components/Salon';
+import { Gallery } from './components/Gallery';
+import { Book } from './components/Book';
 
 const App = () => {
   // JSX
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/salon' element={<Salon />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/book' element={<Book />} />
+      </Routes>
+    </>
   );
 };
 
