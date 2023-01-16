0b0f4bcde40a5ae40e04e67e6c52208d4b1a5ef3
eobcre <eobcre@gmail.com>
2023-01-16 05:59:08 -0500

    nav created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png';
 
 export const Header = () => {
-  return <div></div>;
+  return (
+    <div className='w-screen flex justify-between items-center'>
+      <Link to='/'>
+        <img src={Logo} alt='Logo' width='180' height='29' />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/salon'>Salon</Link>
+          </li>
+          <li>
+            <Link to='/gallery'>Gallery</Link>
+          </li>
+          <li>
+            <Link to='/book'>Book</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
