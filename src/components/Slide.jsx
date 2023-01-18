6651f0dde312f709041498a5afa1c77c9cdf9283
eobcre <eobcre@gmail.com>
2023-01-18 09:41:42 -0500

    applied @apply

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -11,9 +11,9 @@ export const Slide = () => {
   // JSX
   return (
     <EnhancedSwipeableViews enableMouseEvents interval={5000}>
-      <img src={Img1} alt='Image' className='m-auto p-12' />
-      <img src={Img2} alt='Image' className='m-auto p-12' />
-      <img src={Img3} alt='Image' className='m-auto p-12' />
+      <img src={Img1} alt='Image' className='img-st' />
+      <img src={Img2} alt='Image' className='img-st' />
+      <img src={Img3} alt='Image' className='img-st' />
     </EnhancedSwipeableViews>
   );
 };
