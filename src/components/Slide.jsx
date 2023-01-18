374ff53b87a1bb9e45220d2e99db37dc53e7fa4e
eobcre <eobcre@gmail.com>
2023-01-18 09:32:14 -0500

    new component img slide added

---
+++ src/components/Slide.jsx
@@ -0,0 +1,19 @@
+// react-swipeable-views
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+// Images
+import Img1 from '../assets/img1.jpg';
+import Img2 from '../assets/img2.jpg';
+import Img3 from '../assets/img3.jpg';
+
+export const Slide = () => {
+  const EnhancedSwipeableViews = autoPlay(SwipeableViews);
+  // JSX
+  return (
+    <EnhancedSwipeableViews enableMouseEvents interval={5000}>
+      <img src={Img1} alt='Image' className='m-auto p-12' />
+      <img src={Img2} alt='Image' className='m-auto p-12' />
+      <img src={Img3} alt='Image' className='m-auto p-12' />
+    </EnhancedSwipeableViews>
+  );
+};
