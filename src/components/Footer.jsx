e855f991a99fd7f693e61fd28ff40e807225a720
eobcre <eobcre@gmail.com>
2023-01-18 13:49:30 -0500

    layout styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,21 @@ import Instagram from '../assets/ig.png';
 
 export const Footer = () => {
   return (
-    <div className='bg-gray-300 w-full h-80 p-10'>
+    <div className='bg-gray-300 w-full h-72 p-10'>
       <div className='flex justify-between w-2/3 m-auto'>
         <div>
-          <p>Connect with us!</p>
+          <p className='text-xl mb-6'>Connect with us!</p>
           <img src={Instagram} alt='Logo' width='40' height='40' />
         </div>
         <div>
-          <p>Contact</p>
-          <p>Call : xxx-xxx-xxx</p>
-          <p>Email : xxx@xxx.com</p>
+          <p className='text-xl mb-6'>Contact</p>
+          <p className='mt-2 mb-2'>Call : xxx-xxx-xxx</p>
+          <hr className='w-80' />
+          <p className='mt-2 mb-2'>Email : xxx@xxx.com</p>
+          <hr />
         </div>
       </div>
-      <div className='fixed bottom-0 p-10'>
+      <div className='sticky p-12'>
         <small>&copy; 2023 Emmy Manning</small>
       </div>
     </div>
