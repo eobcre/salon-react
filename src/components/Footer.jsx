43df068f93264f28f4cedf4bae63673ddfc0433f
eobcre <eobcre@gmail.com>
2023-01-19 09:46:58 -0500

    layout fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import Instagram from '../assets/ig.png';
 
 export const Footer = () => {
   return (
-    <div className='bg-gray-300 w-full md:h-72 p-10'>
+    <div className='bg-gray-300 w-full pt-10'>
       <div className='md:flex justify-between w-2/3 m-auto'>
         <div>
           <p className='text-xl mb-6'>Connect with us!</p>
@@ -15,14 +15,14 @@ export const Footer = () => {
           </a>
         </div>
         <div>
-          <p className='text-xl mt-20 mb-6 md:-mt-0'>Contact</p>
+          <p className='text-xl mt-12 mb-6 md:-mt-0'>Contact</p>
           <p className='mt-2 mb-2'>Call : xxx-xxx-xxx</p>
           <hr className='w-80' />
           <p className='mt-2 mb-2'>Email : xxx@xxx.com</p>
           <hr />
         </div>
       </div>
-      <div className='sticky p-12'>
+      <div className='sticky p-10'>
         <small>&copy; 2023 Emmy Manning</small>
       </div>
     </div>
