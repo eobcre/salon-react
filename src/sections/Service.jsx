5a55eff1a646b7454b0f208e3f27d754e541dfd0
eobcre <eobcre@gmail.com>
2023-01-22 20:44:05 -0500

    layout styles

--- src/sections/Service.jsx
+++ src/sections/Service.jsx
@@ -1,3 +1,69 @@
 export const Service = () => {
-  return <div className='title-st'>Service</div>;
+  return (
+    <div>
+      <p className='title-st'>Service</p>
+      <div className='w-3/4 mx-auto'>
+        {/* Haircut */}
+        <div className='p-10'>
+          <p className='text-2xl font-bold'>Haircut</p>
+          <ul>
+            <li>
+              <span>Haircut - New Talent</span>
+              <span className='float-right'>$50.00</span>
+            </li>
+            <li>
+              <span>Haircut - Stylist</span>
+              <span className='float-right'>$50.00</span>
+            </li>
+            <li>
+              <span>Haircut - Lead Stlist</span>
+              <span className='float-right'>$50.00</span>
+            </li>
+            <li>
+              <span>Haircut + Scalp Teatment - New Talent</span>
+              <span className='float-right'>$50.00</span>
+            </li>
+            <li>
+              <span>Haircut + Scalp Teatment - Stylist</span>
+              <span className='float-right'>$50.00</span>
+            </li>
+            <li>
+              <span>Haircut + Scalp Teatment - Lead Stylist</span>
+              <span className='float-right'>$50.00</span>
+            </li>
+          </ul>
+        </div>
+        {/* Color */}
+        <div className='p-10'>
+          <p className='text-2xl font-bold'>Color</p>
+          <ul>
+            <li>
+              <span>Full Head Color Service- </span>
+              <span className='float-right'>$70.00 +</span>
+            </li>
+            <li>
+              <span>Full Head Highlights Service</span>
+              <span className='float-right'>$70.00 +</span>
+            </li>
+            <li>
+              <span>Balayage Color Service</span>
+              <span className='float-right'>$70.00 +</span>
+            </li>
+            <li>
+              <span>Foilyage Color Service</span>
+              <span className='float-right'>$70.00 +</span>
+            </li>
+            <li>
+              <span>Bleach Retouch Service</span>
+              <span className='float-right'>$70.00 +</span>
+            </li>
+            <li>
+              <span>Bleach Service, Roots to Ends</span>
+              <span className='float-right'>$70.00 +</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
