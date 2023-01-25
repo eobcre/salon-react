cf08c0bbf0fc6b8e2986c73aac2c99b6713dbdb9
eobcre <eobcre@gmail.com>
2023-01-25 17:22:37 -0500

    title added

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,9 +1,10 @@
-import { Slide } from './Slide';
+import { Slide } from '../components/Slide';
 
 export const Home = () => {
   return (
     <div>
       <Slide />
+      <p className='font-shadow text-6xl text-center'>Love Is In The Air</p>
     </div>
   );
 };
