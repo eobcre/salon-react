f44735d0bde765f948bb6f482c3517b95a65dfba
eobcre <eobcre@gmail.com>
2023-01-25 21:02:48 -0500

    layout fixed

--- src/sections/Book.jsx
+++ src/sections/Book.jsx
@@ -1,78 +1,116 @@
+import Image from '../assets/flower.jpg';
+
 export const Book = () => {
   return (
-    <div className='w-10/12 mx-auto'>
+    <div>
       <p className='title-st'>Booking</p>
       <p className='text-center mb-20'>
         Book your appointment with CAIT SALON. We will get back to you shortly.
       </p>
-      <form>
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Name</legend>
-          <input className='w-80 outline-none' type='text' />
-        </fieldset>
+      <div className='flex justify-center gap-40'>
+        <div>
+          <form>
+            {/* Name */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Name</legend>
+              <input className='w-80 outline-none text-gray-500' type='text' />
+            </fieldset>
 
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Number</legend>
-          <input className='w-80 outline-none' type='text' />
-        </fieldset>
+            {/* Number */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Number</legend>
+              <input className='w-80 outline-none text-gray-500' type='text' />
+            </fieldset>
 
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Email</legend>
-          <input className='w-80 outline-none' type='mail' />
-        </fieldset>
+            {/* Email */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Email</legend>
+              <input className='w-80 outline-none text-gray-500' type='mail' />
+            </fieldset>
 
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Stylist</legend>
-          <select className='w-80 outline-none' name='stylits'>
-            <option value=''>- - - Select Stylist - - -</option>
-            <option value='one'>New Talent</option>
-          </select>
-        </fieldset>
+            {/* Location */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Salon Location</legend>
+              <select
+                className='w-80 outline-none text-gray-500'
+                name='location'
+              >
+                <option value=''>- - - Select Location - - -</option>
+                <option value=''>Rochester</option>
+                <option value=''>Beaver</option>
+                <option value=''>Pittsburgh</option>
+              </select>
+            </fieldset>
 
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Stylist</legend>
-          <select className='w-80 outline-none' name='location'>
-            <option value=''>- - - Select Location - - -</option>
-            <option value=''>Rochester</option>
-            <option value=''>Beaver</option>
-            <option value=''>Pittsburgh</option>
-          </select>
-        </fieldset>
+            {/* Date */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Date</legend>
+              <div className='w-80 text-gray-500'>
+                <input type='date' />
+              </div>
+            </fieldset>
 
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Date</legend>
-          <input type='date' />
-        </fieldset>
+            {/* Stylist */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Stylist</legend>
+              <select
+                className='w-80 outline-none text-gray-500'
+                name='stylits'
+              >
+                <option value=''>- - - Select Stylist - - -</option>
+                <option value='new'>New Talent</option>
+                <option value='stylist'>Stylist</option>
+                <option value='lead'>Lead Stylist</option>
+              </select>
+            </fieldset>
 
-        {/* Checkbox */}
-        <fieldset className='border border-solid rounded border-gray-300 w-1/2 p-3'>
-          <legend className='p-1'>Services</legend>
-          <div>
-            <input type='checkbox' />
-            <label for='haircut' className='p-2'>
-              Haircut
-            </label>
-          </div>
-          <div>
-            <input type='checkbox' />
-            <label for='treatment' className='p-2'>
-              Treatment
-            </label>
-          </div>
-          <div>
-            <input type='checkbox' />
-            <label for='color' className='p-2'>
-              Color
-            </label>
-          </div>
-          <div>
-            <input type='checkbox' />
-            <label for='haircut' className='p-2'>
-              Massage
-            </label>
-          </div>
-        </fieldset>
-      </form>
+            {/* Services */}
+            <fieldset className='border border-solid rounded border-gray-300 p-3'>
+              <legend className='p-1'>Services</legend>
+              <div className='w-80'>
+                <input type='checkbox' />
+                <label htmlFor='haircut' className='p-2'>
+                  Haircut
+                </label>
+              </div>
+              <div>
+                <input type='checkbox' />
+                <label htmlFor='treatment' className='p-2'>
+                  Treatment
+                </label>
+              </div>
+              <div>
+                <input type='checkbox' />
+                <label htmlFor='color' className='p-2'>
+                  Color
+                </label>
+              </div>
+              <div>
+                <input type='checkbox' />
+                <label htmlFor='haircut' className='p-2'>
+                  Massage
+                </label>
+              </div>
+            </fieldset>
+          </form>
+          <button
+            className='border border-gray-300 rounded mt-4 px-6 py-1 hover:bg-gray-100'
+            type='submit'
+          >
+            Send
+          </button>
+        </div>
+
+        <div>
+          <img
+            src={Image}
+            alt='Image'
+            width='300'
+            height='440'
+            className='hidden md:block'
+          />
+        </div>
+      </div>
     </div>
   );
 };
