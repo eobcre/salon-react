4097f0b4d6173ba0b14776e8a3c47a4ae977f3c1
eobcre <eobcre@gmail.com>
2023-01-28 10:15:00 -0500

    contents added

--- src/sections/Salon.jsx
+++ src/sections/Salon.jsx
@@ -1,4 +1,5 @@
 import Floor from '../assets/salon-floor.jpg';
+import Pink from '../assets/pink-hair.jpg';
 
 export const Salon = () => {
   return (
@@ -23,6 +24,26 @@ export const Salon = () => {
           height='240'
           className='mt-10 md:-mt-0 md:ml-10'
         />
+      </div>
+      <div className='md:flex mx-auto mb-10'>
+        <img
+          src={Pink}
+          alt='Image'
+          width='320'
+          height='240'
+          className='mt-10 md:-mt-0 md:mr-10'
+        />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut cum
+          aperiam exercitationem accusamus voluptate ratione, numquam corporis
+          assumenda laudantium soluta minus quo iusto error rerum a itaque ipsam
+          vero impedit.
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+          quidem. Nulla earum voluptates repellat porro veritatis. Assumenda
+          ipsam quisquam vitae harum officiis aperiam reprehenderit dolore
+          recusandae itaque. Dolore, molestiae autem.
+        </p>
       </div>
     </div>
   );
