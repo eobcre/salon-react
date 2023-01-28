a6923e2dec37ea26d9b7eea0259bfec44a6f7080
eobcre <eobcre@gmail.com>
2023-01-28 10:02:49 -0500

    title changed

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div>
       <Slide />
-      <p className='font-shadow text-6xl text-center'>Love Is In The Air</p>
+      <p className='font-shadow text-6xl text-center'>Love Is In The Hair</p>
     </div>
   );
 };
