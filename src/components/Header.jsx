cf6d888381dfbc9d7d1c68ac74bf79d15c6f796c
eobcre <eobcre@gmail.com>
2023-02-06 17:06:38 -0500

    react-icons, creating mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { GrClose, GrMenu } from 'react-icons/gr';
 import Logo from '../assets/logo.png';
 
 export const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavOpen(!navOpen);
+  };
+
+  // JSX
   return (
     <nav>
       <div className='w-10/12 m-auto flex justify-between items-center p-10'>
         <Link to='/'>
           <img src={Logo} alt='Logo' width='180' height='29' />
         </Link>
-
-        <ul className='flex'>
+        {/* Toggle button */}
+        <button onClick={handleToggle} className='md:hidden'>
+          {navOpen ? (
+            <GrClose className='text-2xl' />
+          ) : (
+            <GrMenu className='text-3xl' />
+          )}
+        </button>
+        <ul className={`md:flex ${navOpen ? 'showMenu' : ''}`}>
           <li>
             <Link to='/' className='p-4 hover:opacity-50'>
               Home
