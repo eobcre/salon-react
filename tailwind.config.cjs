88b62f72c84926054b9e28d3cbc546df7505ee4e
eobcre <eobcre@gmail.com>
2023-02-09 00:04:21 -0500

    margin settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         shadow: ['Shadows Into Light', 'cursive'],
       },
+      margin: {
+        '15px': '15px',
+      },
     },
   },
   plugins: [],
