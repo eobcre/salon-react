1f740a0110718fccd9dbb86f6fe8adc70de277d7
eobcre <eobcre@gmail.com>
2023-02-08 21:47:13 -0500

    navbar created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,48 +13,56 @@ export const Header = () => {
   // JSX
   return (
     <nav>
-      <div className='w-10/12 m-auto border-b border-gray-200 flex justify-between items-center p-10'>
-        <Link to='/'>
-          <img src={Logo} alt='Logo' width='180' height='29' />
-        </Link>
-        {/* Toggle button */}
-        <button onClick={handleToggle} className='md:hidden'>
-          {navOpen ? (
-            <GrClose className='text-2xl' />
-          ) : (
-            <GrMenu className='text-3xl' />
-          )}
-        </button>
-        <ul className={`md:flex ${navOpen ? 'showMenu' : ''}`}>
-          <li>
-            <Link to='/' className='p-4 hover:opacity-50'>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to='/salon' className='hover:opacity-50'>
-              Salon
-            </Link>
-          </li>
-          <li>
-            <Link to='/service' className='p-4 hover:opacity-50'>
-              Service
-            </Link>
-          </li>
-          <li>
-            <Link to='/gallery' className='pr-4 hover:opacity-50'>
-              Gallery
-            </Link>
-          </li>
-          <li>
-            <Link
-              to='/book'
-              className='border border-gray-300 rounded p-3 hover:bg-gray-100'
-            >
-              Book now
-            </Link>
-          </li>
-        </ul>
+      <div className='m-auto border-b border-gray-200 p-10 md:w-10/12 md:flex justify-between items-center'>
+        <div className='flex justify-between'>
+          <Link to='/'>
+            <img src={Logo} alt='Logo' width='180' height='29' />
+          </Link>
+          {/* Toggle button */}
+          <button onClick={handleToggle} className='md:hidden'>
+            {navOpen ? (
+              <GrClose className='text-2xl' />
+            ) : (
+              <GrMenu className='text-3xl' />
+            )}
+          </button>
+        </div>
+        <div
+          className={`mt-10 md:block md:mt-0 md:p-0 ${
+            navOpen ? 'block' : 'hidden'
+          }`}
+        >
+          <ul className='grid grid-cols-2 place-items-center gap-4 md:flex'>
+            <li>
+              <Link to='/' className='hover:opacity-50 md:p-4'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/salon' className='hover:opacity-50'>
+                Salon
+              </Link>
+            </li>
+            <li>
+              <Link to='/service' className='hover:opacity-50 md:p-4'>
+                Service
+              </Link>
+            </li>
+            <li>
+              <Link to='/gallery' className='hover:opacity-50 md:pr-4'>
+                Gallery
+              </Link>
+            </li>
+            <li className='mt-2 md:mt-0'>
+              <Link
+                to='/book'
+                className='border border-gray-300 rounded p-3 hover:bg-gray-100'
+              >
+                Book now
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
