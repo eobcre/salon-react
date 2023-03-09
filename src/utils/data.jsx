3142632edfc0cb2776c4b5922f11a42bdb6a6b20
eobcre <eobcre@gmail.com>
2023-03-08 23:04:25 -0500

    new file

---
+++ src/utils/data.jsx
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    name: 'Bombshell',
+    src: '../src/assets/gallery/bombshell.jpg',
+  },
+  {
+    name: 'Ice',
+    src: '../src/assets/gallery/ice.jpg',
+  },
+  {
+    name: 'Precision',
+    src: '../src/assets/gallery/precision.jpg',
+  },
+  {
+    name: 'Red Berry',
+    src: '../src/assets/gallery/red-berry.jpg',
+  },
+  {
+    name: 'Soft Brownie',
+    src: '../src/assets/gallery/soft-brownie.jpg',
+  },
+  {
+    name: 'The Blonde',
+    src: '../src/assets/gallery/the-blonde.jpg',
+  },
+];
