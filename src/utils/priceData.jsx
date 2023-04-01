65f8007d6c540a4997f0b4b890629da808c19d10
eobcre <eobcre@gmail.com>
2023-04-01 09:34:51 -0400

    data for price list

---
+++ src/utils/priceData.jsx
@@ -0,0 +1,32 @@
+export const priceData = [
+  {
+    name: 'Haircut',
+    price: 50,
+    checked: false,
+  },
+  {
+    name: 'Color',
+    price: 60,
+    checked: false,
+  },
+  {
+    name: 'Design Color',
+    price: 80,
+    checked: false,
+  },
+  {
+    name: 'Treatment',
+    price: 50,
+    checked: false,
+  },
+  {
+    name: 'Head Spa',
+    price: 50,
+    checked: false,
+  },
+  {
+    name: 'Hair Set',
+    price: 50,
+    checked: false,
+  },
+];
