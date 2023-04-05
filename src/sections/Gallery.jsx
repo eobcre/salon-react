7798933572e01dcea9f19232c31fa5e74e2b7d3e
eobcre <eobcre@gmail.com>
2023-04-04 21:43:10 -0400

    added key

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -6,7 +6,7 @@ export const Gallery = () => {
       <p className='title-st'>Gallery</p>
       <div className='flex flex-wrap justify-center gap-6'>
         {data.map(({ name, src }, index) => (
-          <figure>
+          <figure key={index}>
             <img src={src} alt={`Image ${index}`} width='340' height='420' />
             <figcaption className='text-center p-4'>{name}</figcaption>
           </figure>
