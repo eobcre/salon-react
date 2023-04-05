3a7970dd07fe7e5579697ae9d46ca324b84ebf0c
eobcre <eobcre@gmail.com>
2023-04-04 21:12:17 -0400

    heroku error debug

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,23 +1,23 @@
 export const data = [
   {
     name: 'Bombshell',
-    src: '../src/assets/gallery/bombshell.jpg',
+    src: '/assets/gallery/bombshell.jpg',
   },
   {
     name: 'Ice',
-    src: '../src/assets/gallery/ice.jpg',
+    src: '/assets/gallery/ice.jpg',
   },
   {
     name: 'Precision',
-    src: '../src/assets/gallery/precision.jpg',
+    src: '/assets/gallery/precision.jpg',
   },
   {
     name: 'Red Berry',
-    src: '../src/assets/gallery/red-berry.jpg',
+    src: '/assets/gallery/red-berry.jpg',
   },
   {
     name: 'Soft Brownie',
-    src: '../src/assets/gallery/soft-brownie.jpg',
+    src: '/assets/gallery/soft-brownie.jpg',
   },
   {
     name: 'The Blonde',
