4fa77c5e095f98081bfb08e2c1f9114f6e84b746
eobcre <eobcre@gmail.com>
2023-04-04 21:55:33 -0400

    path fixed

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,26 +1,26 @@
 export const data = [
   {
     name: 'Bombshell',
-    src: '/gallery/bombshell.jpg',
+    src: '../src/assets/gallery/bombshell.jpg',
   },
   {
     name: 'Ice',
-    src: '/gallery/ice.jpg',
+    src: '../src/assets/gallery/ice.jpg',
   },
   {
     name: 'Precision',
-    src: '/gallery/precision.jpg',
+    src: '../src/assets/gallery/precision.jpg',
   },
   {
     name: 'Red Berry',
-    src: '/gallery/red-berry.jpg',
+    src: '../src/assets/gallery/red-berry.jpg',
   },
   {
     name: 'Soft Brownie',
-    src: '/gallery/soft-brownie.jpg',
+    src: '../src/assets/gallery/soft-brownie.jpg',
   },
   {
     name: 'The Blonde',
-    src: '/gallery/the-blonde.jpg',
+    src: '../src/assets/gallery/the-blonde.jpg',
   },
 ];
