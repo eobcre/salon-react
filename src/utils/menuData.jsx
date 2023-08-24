9496f2cecda15c65f035fc2be9fe7f6d134b2772
eobcre <eobcre@gmail.com>
2023-08-23 22:55:56 -0400

    created data for nav menu

---
+++ src/utils/menuData.jsx
@@ -0,0 +1,27 @@
+export const menuData = [
+  {
+    id: 1,
+    name: 'Home',
+    link: './',
+  },
+  {
+    id: 2,
+    name: 'Salon',
+    link: '/salon',
+  },
+  {
+    id: 3,
+    name: 'Service',
+    link: '/service',
+  },
+  {
+    id: 4,
+    name: 'Gallery',
+    link: '/gallery',
+  },
+  {
+    id: 5,
+    name: 'Booking',
+    link: '/book',
+  },
+];
